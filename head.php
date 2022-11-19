4d2e078e1a512bb8309cb7565b7618a2cad329a6
LuisBaalcazar <luisratoncitobz_3108@hotmail.com>
2022-11-19 18:59:00 -0500

    Cambie contenido

--- head.php
+++ head.php
@@ -111,12 +111,10 @@ width:30%;
 <div id="wrapper"  style="border:5px solid red; position:relative;">
 <div id="main" >
       <div class="navbar">
-        <a href="index.php">EDUCAYSOFT</a>
-        <a href="eys-capacitacion.php">Capacitación</a>
-        <a href="eys-asesoria-tecnica.php">Asesoria Tecnica</a>
-        <a href="eys-investigacion.php">Investigación</a>
-        <a href="eys-desarrollo-web.php">Desarrollo Web</a>
-    	<a href="donacion.php">&#128151; Donaciones</a>
+        <a href="index.php">Inicio</a>
+        <a href="eys-capacitacion.php">Estudios</a>
+        <a href="eys-asesoria-tecnica.php">Conocimiento Informatico</a>
+        <a href="eys-investigacion.php">Hooby</a>
         </div>
     
     
