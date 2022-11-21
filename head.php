cd9a08c8b716c6de56e506d95277f6760959d4ac
LuisBaalcazar <luisratoncitobz_3108@hotmail.com>
2022-11-21 12:40:00 -0500

    Cambie contenido

--- head.php
+++ head.php
@@ -8,7 +8,7 @@
 
 <html lang="es">
 <head>
- <title> Educaysoft </title>
+ <title> Luis Balcazar</title>
 <meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 <link href="images/BalcazarLuis.ico" rel="shortcut icon" type="image/x-icon" />
